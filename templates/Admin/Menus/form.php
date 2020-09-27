e0155cd0f5be7cb1d0bda0f9e8f0af28f7e3e2f8
Giovani O.C.Jr <giovaniocjr@gmail.com>
2020-09-27 16:39:50 -0300

    Updated templates files

--- templates/Admin/Menus/form.php
+++ templates/Admin/Menus/form.php
@@ -1,23 +1,7 @@
 <?php
-
-$crumbs['admin'] = ['title' => 'Admin', 'url' =>  '/admin/dashboard'];
-$crumbs['menus'] = ['title' => 'Menus', 'url' => '/admin/menus'];
-$crumbs['action'] = ['title' => $this->request->getParam('action'), 'url' => null];
-$this->viewVars['crumbs'] =  $crumbs;
-?>
-
-<?= $this->Form->create($row); ?>
-<div class="actions btn-group btn-group-sm">
-    <?= $this->element('Bootstrap4/btn/cancel', ['url' => $crumbs['menus']['url']]); ?>
-    <?= $this->element('Bootstrap4/btn/apply'); ?>
-    <?= $this->element('Bootstrap4/btn/save'); ?>
-</div>
-<div class="row mt-4">
-    <div class="col-12">
-        <?php
-        echo $this->Form->control('title', ['type' => 'text']);
-        echo $this->Form->control('slug', ['type' => 'text']);
-        ?>
-    </div>
-</div>
-<?= $this->Form->end(); ?>
+$colLeft = [
+    'title',
+    'slug'
+];
+$this->set(compact('colLeft', 'colRight'));
+$this->extend('Appearances.Admin/Common/index');
--- templates/Admin/Menus/index.php
+++ templates/Admin/Menus/index.php
@@ -1,35 +1,31 @@
 <?php
-$routeFk = 'target';
 
-$crumbs['admin'] = ['title' => 'Admin', 'url' =>  '/admin/dashboard'];
-$crumbs['fipe'] = ['title' => 'Fipe', 'url' => '/admin/fipe'];
-$crumbs['tipos'] = ['title' => 'Tipos', 'url' => '/admin/fipe/tipos'];
-$crumbs['marcas'] = ['title' => 'Marcas', 'url' => null];
-$this->viewVars['crumbs'] =  $crumbs;
+/**
+ * @var \App\View\AppView $this
+ * @var \App\Model\Entity\row[]|\Cake\Collection\CollectionInterface $rows
+ */
+$trRows[] = 'text-center w-5';
+$trRows[] = 'd-none d-sm-none d-md-none d-lg-table-cell d-xl-table-cell';
+$trRows[] = 'p-0';
 
-$this->extend('/Admin/Common/index'); ?>
-
-<?php $this->start('actions'); ?>
-<div class="btn-group btn-group-sm">
-    <?= $this->element('Bootstrap4/btn/add', ['url' => ['action' => 'add']]) ?>
-    <?= $this->element('Bootstrap4/btn/deleteAll') ?>
-</div>
+$this->assign('title', $menu->title);
+$this->extend('Appearances.Admin/Common/index');
+?>
+<!--table.thead.tr-->
+<?php $this->start('thread'); ?>
+<th class="<?= $trRows[1] ?>"><?= $this->Paginator->sort('id') ?></th>
+<th><?= $this->Paginator->sort('title', __('Title')) ?></th>
+<th class="<?= $trRows[1] ?>"><?= $this->Paginator->sort('item_count','Total de Items') ?></th>
+<th class="actions"><?= __('Actions') ?></th>
 <?php $this->end(); ?>
 
-<?php $this->start('thead'); ?>
-<th style="width:10%;"><?= $this->Paginator->sort('id') ?></th>
-<th><?= $this->Paginator->sort('title', __('Title') ) ?></th>
-<th><?= $this->Paginator->sort('item_count', 'Total') ?></th>
-<th><?= __('Actions') ?></th>
-<?php $this->end(); ?>
-
+<!--table.tbody.tr-->
 <?php $this->start('tbody'); ?>
-<td style="width:10%;">{{id}}</td>
-<td class="p-0 m-0">
-    <a href="<?= $this->Url->build(['controller' => 'Items', 'action' => 'index', $routeFk => '{{id}}']) ?>">
-        {{title}} <p class="small p-0 m-0">{{slug}}</p>
-    </a>
+<td class="<?= $trRows[1] ?>">{{ $this->Number->format($row->id) }}</td>
+<td class="<?= $trRows[2] ?>">{{ $this->Html->link($row->title, ['controller' => 'Items', 'action' => 'index', 'target' => $row->id], ['afterText' => $row->slug])  }}</td>
+<td class="<?= $trRows[1] ?>">{{ h($row->item_count) }}</td>
+<td class="actions">
+    {{ $this->element('bootstrap4/link/view',$url->view) }} -
+    {{ $this->element('bootstrap4/link/edit',$url->edit) }}
 </td>
-<td>{{item_count}}</td>
-<td><?= $this->element('Bootstrap4/link/edit', ['url' => $this->Url->build(['action' => 'edit', $routeFk => '{{tipo_id}}']) . '/{{id}}']) ?></td>
 <?php $this->end(); ?>
